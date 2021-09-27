67e1014360f9d3ee6ee2c047637764a0454cc230
JackLinnehan <linnehjb@bc.edu>
2021-09-27 10:03:39 -0400

    Initial Commit

---
+++ EU/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  EU
+//
+//  Created by John Linnehan on 9/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var members = ["Austria",
+                   "Belgium",
+                   "Bulgaria",
+                   "Croatia",
+                   "Cyprus",
+                   "Czechia",
+                   "Denmark",
+                   "Estonia",
+                   "Finland",
+                   "France",
+                   "Germany",
+                   "Greece",
+                   "Hungary",
+                   "Ireland",
+                   "Italy",
+                   "Latvia",
+                   "Lithuania",
+                   "Luxembourg",
+                   "Malta",
+                   "Netherlands",
+                   "Poland",
+                   "Portugal",
+                   "Romania",
+                   "Slovakia",
+                   "Slovenia",
+                   "Spain",
+                   "Sweden",
+                   "United Kingdom"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return members.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = members[indexPath.row]
+        return cell
+    }
+    
+    
+}
