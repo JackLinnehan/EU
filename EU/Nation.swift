9fc91552265a11fe5a3123dc1a8bbf0cbf2cc967
JackLinnehan <linnehjb@bc.edu>
2021-09-27 12:19:31 -0400

    5.6 EU structs

---
+++ EU/Nation.swift
@@ -0,0 +1,14 @@
+//
+//  Nation.swift
+//  EU
+//
+//  Created by John Linnehan on 9/27/21.
+//
+
+import Foundation
+
+struct Nation {
+    var country: String
+    var capital: String
+    var usesEuro: Bool
+}
